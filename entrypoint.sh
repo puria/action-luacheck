fef9143914d5e21ed726f7701966f2b0f2aa2e5e
Puria Nafisi Azizi <puria@dyne.org>
2021-12-02 02:41:37 +0100

    ci: 💚  Prevent shellcheck false positives

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
+# shellcheck disable=SC2086
 luacheck --formatter plain ${INPUT_LUACHECK_FLAGS} . \
   | reviewdog -efm="%f:%l:%c: %m" \
       -name="luacheck" \
@@ -14,6 +15,7 @@ luacheck --formatter plain ${INPUT_LUACHECK_FLAGS} . \
       -filter-mode="${INPUT_FILTER_MODE}" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
       -level="${INPUT_LEVEL}" \
+      # shellcheck disable=SC2086
       ${INPUT_REVIEWDOG_FLAGS}
 exit_code=$?
 
